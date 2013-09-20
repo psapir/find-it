4d042752d1f27e795aa0e83f6f0fbb8b5add3ebe
Patricio Sapir <psapir@metricsmarketing.com>
2013-09-19 23:33:10 -0400

    populating data

--- FindIt.Api/Controllers/HomeController.cs
+++ FindIt.Api/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using FindIt.Data;
+﻿using FindIt.Api.Models;
+using FindIt.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,8 +12,26 @@ namespace FindIt.Api.Controllers
     {
         public ActionResult Index()
         {
-            using (Entities e = new Entities()) { 
-                e.ContactIndexes.Add(new Models.ContactIndex(){IdContactIndex = Guid.NewGuid(),LastIndexRun=DateTime.Now,mid="1234"});
+            using (Entities e = new Entities()) {
+
+                ContactIndex ci = new Models.ContactIndex() { IdContactIndex = Guid.NewGuid(), LastIndexRun = DateTime.Now, mid = "12355" };
+
+                e.ContactIndexes.Add(ci);
+
+                List<Keyword> keywords = new List<Keyword>();
+
+                keywords.Add(new Keyword(){IdKeyword=Guid.NewGuid(),KeywordText="pato"});
+                keywords.Add(new Keyword(){IdKeyword=Guid.NewGuid(),KeywordText="et"});
+                keywords.Add(new Keyword(){IdKeyword=Guid.NewGuid(),KeywordText="test"});
+                e.SaveChanges();
+
+                for (int i = 0; i < 300; i++)
+			    {
+                    var path = "my emails/path/" + i.ToString();
+                    var res = new Result() { IdResult = Guid.NewGuid(), ContactIndex = ci, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, ResultType = ResultType.email, CustomerKey = Guid.NewGuid().ToString(), Path = path, Keywords = e.Keywords.ToList()};
+			        e.Results.Add(res);
+			    }
+                
                 e.SaveChanges();
             }
             return View();
