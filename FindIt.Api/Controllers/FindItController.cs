387bd0a8ee0face35f39185e4b341c14e133bc47
Patricio Sapir <psapir@metricsmarketing.com>
2013-09-19 22:50:13 -0400

    updating find method

--- FindIt.Api/Controllers/FindItController.cs
+++ FindIt.Api/Controllers/FindItController.cs
@@ -1,4 +1,5 @@
-﻿using FindIt.Data;
+﻿using FindIt.Api.Models;
+using FindIt.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +11,24 @@ namespace FindIt.Api.Controllers
 {
     public class FindItController : ApiController
     {
-        public IEnumerable<object> Find(dynamic criteria)
+        // /api/find
+        public IEnumerable<object> Find(Criteria criteria)
         {
             List<object> results = new List<object>();
 
+            // split keywords by comma will search for multiple keywords.
+            if (criteria.CaseSensitive)
+                criteria.Keyword = criteria.Keyword.ToLower();
+
+            List<string> keywords = criteria.Keyword.Split(',').ToList();
+
             using (Entities db = new Entities())
             { 
-                
+                // Case sensitive settings
+                if(criteria.CaseSensitive)
+                    db.Results.Where(r=>r.ContactIndex.mid == criteria.Mid && r.Keywords.Exists(k=>keywords.Contains(k.KeywordText)));     
+                else
+                    db.Results.Where(r => r.ContactIndex.mid == criteria.Mid && r.Keywords.Exists(k => keywords.Contains(k.KeywordText.ToLower())));     
             }
 
             return results;
---
+++ FindIt.Api/Models/Criteria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FindIt.Api.Models
+{
+    public class Criteria
+    {
+        public String Mid { get; set; }
+        public String Keyword { get; set;} 
+        public bool CaseSensitive{ get; set; }
+    }
+}
